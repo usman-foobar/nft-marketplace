019a546746d71b5fed6adb8be6a59175f6264816
Muhammad Usman <usmansardar247@gmail.com>
2023-12-13 23:19:51 +0500

    feat: mint NFT

---
+++ nodejs-script/smart-contract-interactions/7_mintNft.js
@@ -0,0 +1,21 @@
+const { createNftContractInstance } = require("./configuration");
+const { NftContract } = createNftContractInstance();
+
+// Add a function to mint ERC-721 NFTs (Only Minter)
+
+async function listNftForFixedPrice(toAddress) {
+  const listTransaction = await NftContract.mint(toAddress, {
+    gasLimit: "3000000",
+  });
+  await listTransaction.wait();
+  console.log(
+    `Transaction Completed! Check it out at: https://goerli.etherscan.io/tx//${listTransaction.hash}`
+  );
+}
+
+listNftForFixedPrice("0xE1043012936b8a877D37bd64839544204638d035").catch(
+  (err) => {
+    console.log(err);
+    process.exitCode = 1;
+  }
+);
